1b1e010794e5bd0f6e21f8664943cf12ff7d97b1
Alex Nikolaev <alexn@mm.st>
2022-06-21 18:31:59 +0300

    .

--- internal/server/server.go
+++ internal/server/server.go
@@ -83,8 +83,6 @@ func (s *Server) UpdateJSON(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
 }
 
 func (s *Server) ValueJSON(w http.ResponseWriter, req *http.Request) {
@@ -136,7 +134,6 @@ func (s *Server) Head(w http.ResponseWriter, req *http.Request) {
 func Listen(srv Server, addr string) {
 
 	r := chi.NewRouter()
-	// r.Use(middleware.Compress(5))
 	r.Post("/update/{mtype}/{id}/{value}", srv.Update)
 	r.Get("/value/{mtype}/{id}", srv.Value)
 	r.Get("/", srv.Head)
